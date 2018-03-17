072a66bae78925224dd5712ff4427d483df6a45f
nabilmohamed4302 <37380888+nabilmohamed4302@users.noreply.github.com>
2018-03-17 07:27:03 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+    <!-- Start Script Images Website -->
+    <script src='https://rawgit.com/100px/js/master/read-more.js'/>
+    <script type='text/javascript'>var thumbnail_mode = &quot;no-float&quot; ;
+    summary_noimg = 300; /* حجم النص بدون صورة */
+    summary_img = 220; /* حجم النص مرفوق بصورة*/
+    img_thumb_height = 170; /* طول الصورة في الملخص */
+    img_thumb_width = 270; /* عرض الصورة في الملخص */
+    </script>
+    <!-- End Script Images Website -->
+    <!-- Start Chat -->
+     <script type='text/javascript'>window.$crisp=[];window.CRISP_WEBSITE_ID=&quot;85f6c7bb-c723-4cf2-ad0b-625b95dfd989&quot;;  (function(){d=document;s=d.createElement(&quot;script&quot;);s.src=&quot;https://client.crisp.chat/l.js&quot;;s.async=1;d.getElementsByTagName(&quot;head&quot;)[0].appendChild(s);})();</script>
+    <!-- End Chat -->
+    <!-- Start ToolTip -->
+        <script>
+        $(function () {
+          $(&#39;[data-toggle=&quot;tooltip&quot;]&#39;).tooltip()
+        })
+        </script>
+    <!-- End ToolTip -->
+    <!-- Start Tap -->
+        <script>
+        $(&#39;#myTabs a&#39;).click(function (e) {
+        e.preventDefault()
+        $(this).tab(&#39;show&#39;)
+        })
+        </script>
+     <!-- End Tap -->
+     <!-- Start AddThis -->
+     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a97064b700df865"></script>
+     <!-- End AddThis -->
